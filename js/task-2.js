a7cd0a68899e12302ce5128dd7bb6996119c07b5
OlgaDonchenko <n0931606045@gmail.com>
2024-01-22 20:45:41 +0200

    fixed mist

--- js/task-2.js
+++ js/task-2.js
@@ -1,19 +1,21 @@
 
 class Storage {
-    constructor(items) {
-        this.items = items;
+    #items;
+
+    constructor(firstItems) {
+        this.#items = firstItems;
     }
 
 getItems(){
-    return this.items;
+    return this.#items;
 }
 
 addItem(newItem){
-    this.items.push(newItem);
+    this.#items.push(newItem);
 }
 
 removeItem(itemToRemove){
-    this.items = this.items.filter(item => item !== itemToRemove);
+    this.#items = this.#items.filter(item => item !== itemToRemove);
 }}
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
